d6e15c106ca5261c45ae8f13c3030a58a70099f8
Ly, Kimberly <Kimberly_Ly@intuit.com>
2017-03-16 18:17:24 -0700

    Fixed some styling

---
+++ js/explorefeed.js
@@ -0,0 +1,65 @@
+function readStringFromFileAtPath(pathOfFileToReadFrom) {
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://kimberlyly.github.io/tenkq-occ/" + pathOfFileToReadFrom, false);
+  request.send(null);
+  var returnValue = request.responseText;
+
+  return returnValue;
+  }
+
+
+var instagramData = readStringFromFileAtPath ( "assets/instagramdata.txt" );
+var instagramJson = JSON.parse(instagramData);
+var explorefeed = document.getElementById("explorefeed");
+
+for (i = 0; i < instagramJson.items.length; i++) {
+  explorefeed.innerHTML = explorefeed.innerHTML + '<div class="insta-item"><img src="' + instagramJson.items[i].images.low_resolution.url + '"></div>';
+}
+
+$('#explorefeed').slick({
+  centerMode: true,
+  centerPadding: '15px',
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+/*$('#explorefeed').slickLightbox({
+    src: 'src',
+    itemSelector: '.item img'
+  }); */
